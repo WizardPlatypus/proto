35c59968c4bac2b3ffcbca3e0ed3f7c09e52bf20
WizardPlatypus <f.volik.m@gmail.com>
2024-09-25 15:12:30 +0300

    Hello Rocket!
    Signed-off-by: WizardPlatypus <f.volik.m@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello there!");
+#[macro_use] extern crate rocket;
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    pretty_env_logger::init();
+
+    log::info!("Launching...");
+    let _rocket = rocket::build()
+        .mount("/hello", routes![world])
+        .launch()
+        .await?;
+
+    Ok(())
 }
